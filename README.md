b3e5aafaeb3b213f865ce0b141d0610ab410ade5
Aurelien-jcb <jacob-aurelien@hotmail.fr>
2021-02-01 17:30:23 +0100

    Modification des fichiers

---
+++ README.md
@@ -0,0 +1,35 @@
+# Symfony basic
+
+Ce projet permet de découvrir Symfony
+
+## BDD
+Utilisateur
+- nom
+- prénom
+- email
+- type: élève, prof, client
+
+ou
+
+Elèves
+- nom
+- prénom
+- email
+
+Prof
+- nom
+- prénom
+- email
+
+Client
+- nom 
+- prénom
+- email
+- responsable
+
+Projet
+- nom
+- description
+client
+prof
+élèves
---
+++ src/Form/ProjectForStudent.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Project;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProjectType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Project::class,
+            // Désactive la validation HTML5
+            'attr' => [
+                'novalidate' => 'novalidate',
+            ]
+        ]);
+    }
+}
